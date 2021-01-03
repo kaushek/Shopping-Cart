9fb90ffb0731bc8c6e459d8f0693b48541ad7d37
kaushek <kaushek.kr@gmail.com>
2021-01-03 17:45:14 +0530

    Updated Dockerfile, index.js, package.json

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # Use the official lightweight Node.js 12 image.
 # https://hub.docker.com/_/node
-FROM node:12-slim
+FROM node:14.15.3
 
 # Create and change to the app directory.
-WORKDIR /usr/src/app
+WORKDIR /app
 
 # Copy application dependency manifests to the container image.
 # A wildcard is used to ensure both package.json AND package-lock.json are copied.
@@ -12,9 +12,11 @@ COPY package*.json ./
 
 # Install production dependencies.
 RUN npm install --only=production
+RUN npm install express
+RUN npm install request
 
 # Copy local code to the container image.
-COPY index.js /app/index.js
+COPY . .
 
 # Run the web service on container startup.
-CMD [ "npm", "start" ]
+CMD node index.js
--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-var express = require('express')
-var app = express()
+const { json } = require("express");
+const express = require("express");
+let app = new express();
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+let port = 12345;
+app.listen(port, function() {
+    var request = require('request');
+    request.get({
+      url: 'https://nalinindika90-eval-prod.apigee.net/catalog-reader/get-catalog?apikey=1hUf1UjGYKXYZA1cNExgcgbqYGhh8Cl8',
+     // url: 'http://34.123.46.214',
+      json: true,
+     // headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        // data is already parsed as JSON:
+        console.log(data);
+       // var resoponse = JSON.parse(res);
+        var price = 0;
+        var cartArray = [];
+        for(var items in data.items){
+          var key = Object.keys(data.items[items]);
+          var value = Object.values(data.items[items]);
+          if (value[0] == "ToothBrush" || value[0]== "soap"){
+            price = parseFloat(value[1]) + price;
+            cartArray.push(data.items[items]);
+          }
+        }
 
-//Launch listening server on port 8081
-app.listen(8080);
- 
-module.exports = app;
+        var cartData = {  
+          "OrderId": Math.floor(Math.random() * 10000),
+          "Items": JSON.parse(JSON.stringify(cartArray)),
+          "TotalPrice": price,
+          "PaymentMethod": "card"
+        };
+        request.post('http://localhost:12345', {cartData});
+
+
+        app.get("/", function(req, res) {
+        res.send(cartData);
+      });
+      
+      }
+  });
+
+});
