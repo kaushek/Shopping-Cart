3389105659cddf0e47829c3270ea901d02f68747
kaushek <kaushek.kr@gmail.com>
2021-01-07 14:14:55 +0000

    edit index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { json } = require("express");
 const express = require("express");
 let app = new express();
+var cartData = [];
+var result = 0
+
+app.post("/", function(req, res) {
+  res.send(result.toString());
+});
+
+app.get("/", function(req, res) {
+  res.send(cartData);
+});
+
+
 
 let port = 8080;
 app.listen(port, function() {
@@ -30,20 +42,75 @@ app.listen(port, function() {
           }
         }
 
-        var cartData = {  
+        cartData = {  
           "OrderId": Math.floor(Math.random() * 10000),
           "Items": JSON.parse(JSON.stringify(cartArray)),
           "TotalPrice": price,
           "PaymentMethod": "card"
         };
-       // request.post('http://35.193.132.159', {cartData});
 
+        var BillingData = {
+          "userId": (Math.floor(Math.random() * 10000)).toString(),
+          "items": JSON.parse(JSON.stringify(cartArray)),
+          "paymentMethod": "credit-card"
+        };
+       
+        console.log(BillingData);
+        request.post({
+           url: 'https://umrfirdausi-eval-prod.apigee.net/api/billing-svc/generate-invoice?apikey=ODsnKFJAj6j1ZcZuxd5T7KLMrsidUtTG',
+           json: true,
+           body: BillingData
+           
+         },
+         function(error, response, user){        
+           console.log(response.statusCode);
+           if (response.statusCode == 200)
+           {
+             //call put order
+             request(
+              { method: 'PUT'
+              , uri: 'https://nalinindika90-eval-prod.apigee.net/order-persistor/put-order?apikey=AE4qCAAOeMflzIfniY3qaSBInVHvMGMa'
+              , multipart:
+                [ { 'content-type': 'application/json'
+                  ,  body: JSON.stringify( {
+                      "user_id": Math.floor(Math.random() * 10000),
+                      "items": "ToothBrush,soap",
+                      "total_price": price
+                    })
+                  }          
+                ]
+              }
+            , function (error, response, body) {        
+                  console.log(response.statusCode);                    
+              }
+            )
+           }
+         }
+       )
 
-        app.get("/", function(req, res) {
-        res.send(cartData);
-      });
-      
+       
+
+       
+     
       }
   });
+  var num1 = Number(1);
+  var num2 = Number(2);
+   
+  result = num1 + num2 ;
+   
 
+var clientServerOptions = {
+   uri: 'http://35.193.132.159/',
+   body: JSON.stringify(result),
+   method: 'POST',
+   headers: {
+       'Content-Type': 'application/json'
+   }
+}
+request(clientServerOptions, function (error, response) {
+   console.log(response.statusCode);
+   return;
 });
+});
+
